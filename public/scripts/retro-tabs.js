0799795f6c9c2a38441b6e5f51ccfa6c4aac5355
Parimal Aswani <paswani@gmail.com>
2018-06-25 18:45:51 +0800

    Switch from tabs and grid view based on window sizing

---
+++ public/scripts/retro-tabs.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('retroman')
+  .directive("retroTabs", ['$window', '$compile', '$timeout', function($window, $compile, $timeout) {
+    return {
+        restrict: 'E',
+        link: link,
+        templateUrl: 'html/retro-tabs.html'
+        }
+    
+    function link(scope, element, attrs) {      
+        function checkTemplateVisible(event) {
+            //console.debug("Window visibility check " + $window.innerWidth);
+            
+             //use $timeout to make sure $apply called in a time manner
+             $timeout(function(){
+
+                  //pageYoffset is equal to window scroll top position
+                  if($window.innerWidth < 850){
+                      scope.includeMobileTemplate = true;
+                      scope.includeDesktopTemplate = false;
+                  }else{
+                      scope.includeMobileTemplate = false;
+                      scope.includeDesktopTemplate = true;
+                  }
+             })
+
+        }
+
+        angular.element($window).on('scroll', checkTemplateVisible)
+        angular.element($window).on('resize', checkTemplateVisible)
+
+        function cleanUp() {
+          console.debug("retro-tabs cleanup");
+          angular.element($window).off('resize', checkTemplateVisible);
+          angular.element($window).off('scroll', checkTemplateVisible);
+        }
+        
+        scope.$on('$destroy', cleanUp);
+        checkTemplateVisible();
+    }
+}]);
