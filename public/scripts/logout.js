5980ffe8dd5ead2cd088584affd2d21403f8524f
Johnson Koh <johnsonkoh@Johnsons-MacBook-Pro.local>
2018-09-12 13:25:26 +0800

    Improved front page graphics and fixed nav bar

--- public/scripts/logout.js
+++ public/scripts/logout.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('retroman')
- .controller('LogoutController', function ($scope, $location) {
-   console.debug('Logout');
-   firebase.auth().signOut();
-   $location.path('/login');
+ .controller('LogoutController', function ($scope, $rootScope, $location) {
+     $rootScope.showAdmin = false;
+     console.debug('Logout');
+     firebase.auth().signOut();
+     $location.path('/login');
  });
